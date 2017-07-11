cb8579f8f869a7c5b0631243970f91160b7a9523
hukuolin@outlook.com <2468101214hkl>
2017-07-11 22:50:14 +0800

    创建作业调度实体类库公共调用

---
+++ Demo/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.b = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.a = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.c = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.d = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.a,
+            this.b,
+            this.c,
+            this.d});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(467, 253);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // b
+            // 
+            this.b.DisplayIndex = 0;
+            this.b.Text = "描述";
+            this.b.Width = 200;
+            // 
+            // a
+            // 
+            this.a.DisplayIndex = 1;
+            this.a.Text = "别名";
+            this.a.Width = 200;
+            // 
+            // c
+            // 
+            this.c.Text = "发送";
+            // 
+            // d
+            // 
+            this.d.Text = "接收";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 253);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "C# Monitor NIC Traffic.";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader a;
+        private System.Windows.Forms.ColumnHeader b;
+        private System.Windows.Forms.ColumnHeader c;
+        private System.Windows.Forms.ColumnHeader d;
+    }
+}
+
---
+++ Demo/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public partial class Win32Native
+        {
+            [StructLayout(LayoutKind.Sequential)]
+            public class MIB_IF_ROW2
+            {
+                public long InterfaceLuid;
+                public int InterfaceIndex;
+                public Guid GUID;
+                [MarshalAs(UnmanagedType.ByValArray, SizeConst = 514)]
+                private byte[] bAlias;
+                public string Alias
+                {
+                    get
+                    {
+                        return Encoding.Unicode.GetString(this.bAlias);
+                    }
+                }
+                [MarshalAs(UnmanagedType.ByValArray, SizeConst = 514)]
+                private byte[] bDescription;
+                public string Description
+                {
+                    get
+                    {
+                        return Encoding.Unicode.GetString(this.bDescription);
+                    }
+                }
+                public int PhysicalAddressLength;
+                [MarshalAs(UnmanagedType.ByValArray, SizeConst = 32)]
+                public byte[] PhysicalAddress;
+                [MarshalAs(UnmanagedType.ByValArray, SizeConst = 32)]
+                public byte[] PermanentPhysicalAddress;
+                public int Mtu;
+                public int Type;
+                public int TunnelType;
+                public int MediaType;
+                public int PhysicalMediumType;
+                public int AccessType;
+                public int DirectionType;
+                [MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)]
+                public byte[] InterfaceAndOperStatusFlags;
+                public int OperStatus;
+                public int AdminStatus;
+                public int MediaConnectState;
+                public Guid NetworkGuid;
+                public int ConnectionType;
+                public long TransmitLinkSpeed;
+                public long ReceiveLinkSpeed;
+                public long InOctets;
+                public long InUcastPkts;
+                public long InNUcastPkts;
+                public long InDiscards;
+                public long InErrors;
+                public long InUnknownProtos;
+                public long InUcastOctets;
+                public long InMulticastOctets;
+                public long InBroadcastOctets;
+                public long OutOctets;
+                public long OutUcastPkts;
+                public long OutNUcastPkts;
+                public long OutDiscards;
+                public long OutErrors;
+                public long OutUcastOctets;
+                public long OutMulticastOctets;
+                public long OutBroadcastOctets;
+                public long OutQLen;
+            }
+
+            [StructLayout(LayoutKind.Sequential)]
+            public struct MIB_IF_TABLE2
+            {
+                public int NumEntries;
+                public MIB_IF_ROW2[] Table;
+            }
+
+            [DllImport("Iphlpapi.dll", SetLastError = true)]
+            public static unsafe extern int GetIfTable2(ref int Table);
+
+            [DllImport("Kernel32.dll", SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            public static unsafe extern bool RtlMoveMemory(ref int Destination, int Source, int Length);
+
+            public const int NULL = 0;
+            public const int NO_ERROR = 0;
+        }
+        public static unsafe Win32Native.MIB_IF_TABLE2 GetMIB2Interface()
+        {
+            int ptr_Mit2 = Win32Native.NULL;
+            Win32Native.MIB_IF_TABLE2 s_Mit2 = new Win32Native.MIB_IF_TABLE2(); // create empty mit2.
+            if (Win32Native.GetIfTable2(ref ptr_Mit2) != Win32Native.NO_ERROR)
+                return s_Mit2;
+            if (Win32Native.RtlMoveMemory(ref s_Mit2.NumEntries, ptr_Mit2, 4) && s_Mit2.NumEntries > 0)
+            {
+                ptr_Mit2 = ptr_Mit2 + 8; // ptr_Mit2 += 8; offset pointer to MIB_IF_TABLE2::Table
+                int dwBufferSize = 1352; // sizeof(Win32Native.MIB_IF_ROW2);
+                s_Mit2.Table = new Win32Native.MIB_IF_ROW2[s_Mit2.NumEntries];
+                for (int i = 0; i < s_Mit2.NumEntries; i++)
+                {
+                    IntPtr ptr = (IntPtr)(ptr_Mit2 + (i * dwBufferSize));
+                    s_Mit2.Table[i] = (Win32Native.MIB_IF_ROW2)
+                        Marshal.PtrToStructure(ptr, typeof(Win32Native.MIB_IF_ROW2));
+                }
+            }
+            return s_Mit2;
+        }
+
+        private unsafe void Form1_Load(object sender, EventArgs e)
+        {
+            Win32Native.MIB_IF_TABLE2 s_Mit2 = GetMIB2Interface();
+            for (int i = 0; i < s_Mit2.NumEntries; i++)
+            {
+                listView1.Items.Add(
+                    new ListViewItem(
+                        new string[]
+                            {
+                                s_Mit2.Table[i].Description,
+                                s_Mit2.Table[i].Alias,
+                                s_Mit2.Table[i].InOctets.ToString(),
+                                s_Mit2.Table[i].OutOctets.ToString()
+                            }
+                        )
+                    );
+            }
+        }
+    }
+}
