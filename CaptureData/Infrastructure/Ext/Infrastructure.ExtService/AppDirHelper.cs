65b533fd90e508dac915d0bba74a8646cd8fa487
hukuolin@outlook.com <2468101214hkl>
2017-11-01 00:09:35 +0800

    [2017-10-31] xml文件数据读取  配置项内，以及配置项的值读取形式

---
+++ CaptureData/Infrastructure/Ext/Infrastructure.ExtService/AppDirHelper.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Infrastructure.ExtService
+{
+    public enum AppCategory
+    {
+        [Description("Web应用")]
+        WebApp=1,
+        [Description("桌面应用")]
+        WinApp=2
+    }
+    public class AppDirHelper
+    {
+        public string GetAppDir(AppCategory category) 
+        {
+            string dir = string.Empty;
+            switch (category)
+            {
+                case AppCategory.WebApp:
+                    dir = GetWebAppDir();
+                    break;
+                case AppCategory.WinApp:
+                    dir = GetWinAppDir();
+                    break;
+                default:
+                    break;
+            }
+            return dir;
+        }
+        string GetWinAppDir() 
+        {
+            string path = this.GetType().Assembly.Location;
+            DirectoryInfo di = new DirectoryInfo(path);
+            return di.Parent.FullName;
+        }
+        string GetWebAppDir() 
+        {
+            string dir = AppDomain.CurrentDomain.BaseDirectory;
+            return dir;
+        }
+    }
+    public class NowAppDirHelper
+    {
+        /// <summary>
+        /// 根据提供的程序类型【web或者桌面应用程序】获取当前目录
+        /// </summary>
+        /// <param name="category"></param>
+        /// <returns></returns>
+        public static string GetNowAppDir(AppCategory category)
+        {
+            return (new AppDirHelper()).GetAppDir(category);
+        }
+    }
+}
---
+++ CaptureData/Infrastructure/Ext/Infrastructure.ExtService/XmlFileHelper.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace Infrastructure.ExtService
+{
+    public class XmlFileHelper
+    {
+        /// <summary>
+        /// 读取节点下各项属性内容 如：  <add key="key" value="value"/>
+        /// </summary>
+        /// <param name="xmlFile"></param>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        public static List<Dictionary<string, string>> ReadXmlNodeItemInAttribute(string xmlFile, string node)
+        {
+            if (string.IsNullOrEmpty(xmlFile))
+            {
+                return null;
+            }
+            if (!File.Exists(xmlFile))
+            {
+                return null;
+            }
+            XmlDocument doc = new XmlDocument();
+            doc.Load(xmlFile);
+            XmlNode xn = doc.SelectSingleNode(node);
+            if (xn == null)
+            {
+                return null;
+            }
+            //通过提供的节点找不到任何子节点数据
+            XmlNodeList xnl = xn.ChildNodes;
+            List<Dictionary<string, string>> nodes = new List<Dictionary<string, string>>();
+            foreach (XmlNode item in xnl)
+            {
+                if (item.NodeType == XmlNodeType.Comment) { continue; }//忽略注释列
+                Dictionary<string, string> nodeItem = new Dictionary<string, string>();
+                XmlAttributeCollection attrs = item.Attributes;
+                foreach (XmlAttribute attr in attrs)
+                {
+                    nodeItem.Add(attr.Name, attr.Value);
+                }
+                nodes.Add(nodeItem);
+            }
+            return nodes;
+        }
+        /// <summary>
+        /// 读取xml文件下配置节点的文本  如:<key>value</key>
+        /// </summary>
+        /// <param name="xmlFile"></param>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        public static Dictionary<string, string> ReadXmlNodeItemInText(string xmlFile, string node)
+        {
+            if (string.IsNullOrEmpty(xmlFile))
+            {
+                return null;
+            }
+            if (!File.Exists(xmlFile))
+            {
+                return null;
+            }
+            XmlDocument doc = new XmlDocument();
+            doc.Load(xmlFile);
+            XmlNode xn = doc.SelectSingleNode(node);
+            if (xn == null)
+            {
+                return null;
+            }
+            //通过提供的节点找不到任何子节点数据
+            XmlNodeList xnl = xn.ChildNodes;
+            Dictionary<string, string> entity = new Dictionary<string, string>();
+            foreach (XmlNode item in xnl)
+            {
+                if (item.NodeType == XmlNodeType.Comment) { continue; }//忽略注释列
+                entity.Add(item.Name, item.InnerText);
+               // string item.Name
+               // nodes.Add(nodeItem);
+            }
+            return entity;
+        }
+    }
+}
