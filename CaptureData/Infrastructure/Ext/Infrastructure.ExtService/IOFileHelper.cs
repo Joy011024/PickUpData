afec30992be07b2b35cc9d68ef3c4a30932e0356
hukuolin@outlook.com <2468101214hkl>
2017-12-18 20:40:20 +0800

    2017-12-18 文件完善

---
+++ CaptureData/Infrastructure/Ext/Infrastructure.ExtService/IOFileHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+namespace Infrastructure.ExtService
+{
+    public class IOFileHelper
+    {
+        /// <summary>
+        /// 文件拷贝【不能为相同路径】
+        /// </summary>
+        /// <param name="fileFullName">原路径</param>
+        /// <param name="newFileFullName">新路径</param>
+        public static void FileCopy(string fileFullName,string newFileDir,string newFileName)
+        {
+            fileFullName = fileFullName.Trim();
+            string newFileFullName = newFileDir + "/" + newFileName;
+            newFileFullName = newFileFullName.Trim();
+            if (fileFullName.ToLower() == newFileFullName.ToLower())
+            {
+                return;
+            }
+            if (File.Exists(newFileFullName))
+                File.Delete(newFileFullName);
+            if (!Directory.Exists(newFileDir))
+                Directory.CreateDirectory(newFileDir);
+            File.Copy(fileFullName, newFileFullName);
+        }
+    }
+}
