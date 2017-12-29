ffda0fd67a1a72127079db62f024a105fe505307
hukuolin@outlook.com <hukuolin@outlook.com>
2017-12-29 09:40:56 +0800

    2017-12-29  增加人员管理类库【init】

---
+++ HRApp/HRApp.Model/Entity/CategoryItems.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.CommonData;
+using System.ComponentModel;
+namespace HRApp.Model
+{
+    public class CategoryItems:CategoryData
+    {
+        [Description("配置项被引用次数")]
+        public int ItemUsingSize { get; set; }
+        [Description("内容项的描述")]
+        public string ItemDesc { get; set; }
+    }
+}
---
+++ HRApp/HRApp.Model/SqlCmd/Create/HrApp.sql
@@ -0,0 +1,13 @@
+CREATE TABLE [dbo].[CategoryItems](
+	[ID] [int] NOT NULL primary key,
+	[Name] [nvarchar](32) NULL,
+	[ParentID] [int] NULL,
+	[ParentCode] [nvarchar](64) ,
+	[Code] [nvarchar](64) not NULL,--UI层次没有设定的话通过文本转拼音进行设置
+	[Sort] [int] NULL,
+	[IsDelete] [bit] NULL,
+	ItemUsingSize int default(1),--改项被引用的次数【进行删除时的判断校验】
+	[CreateTime] [datetime] NULL,
+	[NodeLevel] [int] default(0),
+	ItemDesc nvarchar(1024)
+)  
