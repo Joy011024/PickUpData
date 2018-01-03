9d4b7d6ffcaed08fbb78582c52d11e0b5b233616
hukuolin@outlook.com <hukuolin@outlook.com>
2018-01-03 09:45:47 +0800

    2018-01-03 漏提交文件

---
+++ HRApp/HRApp.Web/Controllers/AppSettingManageController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Common.Data;
+namespace HRApp.Web.Controllers
+{
+    public class AppSettingManageController : Controller
+    {
+        //
+        // GET: /AppSettingManage/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult SaveAppSetting() 
+        {
+            JsonData json = new JsonData();
+            return Json(json);
+        }
+    }
+}
