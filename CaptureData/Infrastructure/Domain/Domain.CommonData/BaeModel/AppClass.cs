1a26bb4c2e9ce85285340dae32a0392b198b8b90
hukuolin@outlook.com <2468101214hkl>
2018-01-23 00:18:02 +0800

    2018-01-23 系统版本控制类

---
+++ CaptureData/Infrastructure/Domain/Domain.CommonData/BaeModel/AppClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace Domain.CommonData
+{
+    [Description("int 主键的公共类")]
+    public class BaseIntPrimary 
+    {
+        public int Id { get; set; }
+
+    }
+    public class BaseLogicWithIntPRimary:BaseIntPrimary
+    {
+        public int IsDelete { get; set; }
+    }
+    public class App : BaseLogicWithIntPRimary
+    {
+        public string AppName { get; set; }
+        public string AppCode { get; set; }
+    }
+    public class AppVer:BaseLogicWithIntPRimary
+    {
+        public string AppVersion { get; set; }
+        public string AppId { get; set; }
+        public bool IsNowVersion { get; set; }
+        public DateTime CreateTime { get; set; }
+    }
+}
---
+++ CaptureData/Infrastructure/Domain/Domain.CommonData/BaeModel/DescriptionSortAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Domain.CommonData
+{
+    public class DescriptionSortAttribute : DescriptionAttribute
+    {
+        public int Sort { get; set; }
+        public DescriptionSortAttribute(string description, int sort = 0)
+        {
+            Sort = sort;
+            DescriptionValue = description;
+        }
+    }
+}
