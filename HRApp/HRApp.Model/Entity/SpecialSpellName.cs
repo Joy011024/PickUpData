7cf38b771ffd0accc6b01893e25c7fa3e2bc7dc3
hukuolin@outlook.com <2468101214hkl>
2018-02-01 21:59:27 +0800

    2018-02-01 本地漏提交文件

---
+++ HRApp/HRApp.Model/Entity/SpecialSpellName.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.CommonData;
+namespace HRApp.Model
+{
+    [Description("生僻字转拼音")]
+    public class SpecialSpellName:SpellChineseWord
+    {
+        public int Id { get; set; }
+        public int IsDeleted { get; set; }
+        [Description("拼音错误")]
+        public bool IsErrorSpell { get; set; }
+    }
+    [Description("汉字可能的拼音")]
+    public class MaybeSpellName:SpellChineseWord
+    {
+        public Guid Id { get; set; }
+        public DateTime CreateTime { get; set; }
+        public int CreateDayInt { get; set; }
+        [Description("是否生僻字")]
+        public bool IsSpecialChinese { get; set; }
+        //
+        string cmd = @"INSERT INTO  [dbo].[MaybeSpellName]   ([Id],[Name],[Code],[CreateTime],[CreateDayInt])
+     VALUES  ({Id},{Name},{Code},{CreateTime},{CreateDayInt})";
+        public void SaveMaybeSpecialChinese(string name,string code) 
+        {
+            string cmd = @"EXEC	@return_value = [dbo].[SP_VerifyAndAddMaybeSpellName]
+		@Name = N'篆',
+		@Code = N'Zhuan',
+		@result = @result OUTPUT";
+        }
+        public void InitParam() 
+        {
+            Id = Guid.NewGuid();
+            CreateTime = DateTime.Now;
+            CreateDayInt = int.Parse(CreateTime.ToString("yyyyMMdd"));
+        }
+    }
+}
