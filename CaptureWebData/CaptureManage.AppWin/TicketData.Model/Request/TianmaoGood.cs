07859c8271ecd0b535cd04068376267902f08255
hukuolin@outlook.com <hukuolin@outlook.com>
2018-02-02 15:34:35 +0800

    2018-02-02 天猫商品初步分析

--- CaptureWebData/CaptureManage.AppWin/TicketData.Model/Request/TianmaoGood.cs
+++ CaptureWebData/CaptureManage.AppWin/TicketData.Model/Request/TianmaoGood.cs
@@ -9,8 +9,15 @@ namespace TicketData.Model
     [Description("天猫商品数据信息采集product-iWrap 层下productImg-wrap")]
     public class TianmaoGood
     {
+        [Description("商品链接【productImg 的href属性补充http:】")]
+        public string GoodHref { get; set; }
+        [Description("商品图片 IMG属性src")]
         public string productImg { get; set; }
         public string productPrice { get; set; }
+        [Description("单价")]
+        public float GoodPrice { get; set; }
+        [Description("币种")]
+        public char PriceUnit { get; set; }
         public string productTitle { get; set; }
         public string productShop { get; set; }
         public string productStatus { get; set; }
