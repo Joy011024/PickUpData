7838031b8eca191e4a60a550bfef1fbaf7bed030
hukuolin@outlook.com <2468101214hkl>
2018-03-01 00:07:45 +0800

    2018-03-01 上传文件功能 【虽然在UI可以多次选择文件，但是在此处上传文件时是进行多次上传到后台】

---
+++ HRApp/HRApp.ApplicationService/FileService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRApp.ApplicationService
+{
+    public class FileHelperService
+    {
+        public void SaveFile(string fileDir) 
+        {
+        
+        }
+    }
+    public class FileService
+    {
+
+    }
+}
--- HRApp/HRApp.Web/Controllers/FileHelperController.cs
+++ HRApp/HRApp.Web/Controllers/FileHelperController.cs
@@ -26,8 +26,30 @@ namespace HRApp.Web.Controllers
                 json.Success = false;
                 return Json(json);
             }
+            string dir=Server.MapPath("/UploadFile/")+DateTime.Now.ToString(Common.Data.CommonFormat.DateIntFormat);
+            if (!System.IO.Directory.Exists(dir))
+            {
+                System.IO.Directory.CreateDirectory(dir);
+            }
+            int succNum=0;
+            json.Message = string.Format("本次总共上传文件 {0} 份",files.Count);
             //进行文件存储
-
+            for (int i = 0; i < files.Count; i++)
+            {
+                try
+                {
+                    HttpPostedFileBase hf = files[i];
+                    string[] fileInfo = hf.FileName.Split('.');
+                    hf.SaveAs(dir + "/" + DateTime.Now.ToString(Common.Data.CommonFormat.DateTimeIntFormat) + "." + fileInfo[fileInfo.Length - 1]);
+                    succNum++;
+                    json.Message += string.Format("<br/>第{0}张上传成功。", i + 1);
+                }
+                catch (Exception ex)
+                {
+                    json.Message += string.Format("<br/>第{0}张上传失败。" ,i+1);
+                }
+            }
+            json.Success = succNum > 0;
             return Json(json);
         }
     }
