c140e6926858227c5956f5fa6c4b3006b95bcb44
hukuolin@outlook.com <2468101214hkl>
2018-03-15 21:59:58 +0800

    2018-03-15 对于查询的qq数据增加采集日期 日期戳同步脚本

--- CaptureData/Infrastructure/Domain/Domain.CommonData/SqlCmd/UpdateSp/V1.5/InitGatherImageProperty.sql
+++ CaptureData/Infrastructure/Domain/Domain.CommonData/SqlCmd/UpdateSp/V1.5/InitGatherImageProperty.sql
@@ -2,3 +2,4 @@
  update  TecentQQData  set   imgtype=0 where len(headimageurl)>0 and imgtype is null;
  update TecentQQData  set GatherImageErrorNum=0 where GatherImageErrorNum is null;
  update TecentQQData  set IsGatherImage=0 where IsGatherImage is null;
+ update TecentQQData  set  DayInt=convert(int, convert(varchar(8), createtime,112) )where DayInt=0
