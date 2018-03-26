279a3917d2173cd2838aa56f1e1d3b164679e510
hukuolin@outlook.com <hukuolin@outlook.com>
2018-03-26 17:14:38 +0800

    2018-03-26  商品入库管理模块

---
+++ HRApp/HRApp.Web/Controllers/OrderManageController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.EnterpriseServices;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HRApp.Web.Controllers
+{
+    [Description("订单元数据管理")]
+    public class OrderManageController : Controller
+    {
+        //
+        // GET: /OrderManage/
+        [Description("货物维护管理")]
+        public ActionResult GoodsManage()
+        {
+            return View();
+        }
+
+    }
+}
