bea6c37922dd11058a09e7755402ead460afad0e
hukuolin@outlook.com <hukuolin@outlook.com>
2018-04-03 18:19:42 +0800

    2018-04-03 ioc注入组合 dal，bll

--- HRApp/HRApp.Web/Global.asax.cs
+++ HRApp/HRApp.Web/Global.asax.cs
@@ -59,6 +59,10 @@ namespace HRApp.Web
             }
             static void OrmIocFactory()
             {
+                if (propertyVal.Count > 0)
+                {//存储字典非空出来 
+                
+                }
                 string connString = InitAppSetting.LogicDBConnString;
                 InterfaceIocHelper ioc = new InterfaceIocHelper();
                 string dir = NowAppDirHelper.GetNowAppDir(AppCategory.WebApp);
@@ -73,20 +77,51 @@ namespace HRApp.Web
                 #region dal层属性
                 IAppRepository appDal = ioc.IocConvert<IHRApp.Infrastructure.IAppRepository>(dllDir, mvc[MvcLevel.DAL].AssemblyName, mvc[MvcLevel.DAL].Namespace, typeof(AppRepository).Name);
                 ioc.IocFillProperty<IAppRepository, IAppRepository>(appDal, propertyVal);
-                IAppSettingRepository appSettingDal = ioc.IocConvert<IHRApp.Infrastructure.IAppSettingRepository>(dllDir, mvc[MvcLevel.DAL].AssemblyName, mvc[MvcLevel.DAL].Namespace, typeof(AppSettingRepository).Name);
+                IAppSettingRepository appSettingDal = ioc.IocConvert<IAppSettingRepository>(dllDir, mvc[MvcLevel.DAL].AssemblyName, mvc[MvcLevel.DAL].Namespace, typeof(AppSettingRepository).Name);
                 ioc.IocFillProperty(appSettingDal, propertyVal);
+                IMenuRepository menuDal = ioc.IocConvert<IMenuRepository>(dllDir, mvc[MvcLevel.DAL].AssemblyName, mvc[MvcLevel.DAL].Namespace, typeof(MenuRepository).Name);
+                ioc.IocFillProperty(menuDal, propertyVal);
+                IContactDataRepository contacterDal = ioc.IocConvert<IContactDataRepository>(dllDir, mvc[MvcLevel.DAL].AssemblyName, mvc[MvcLevel.DAL].Namespace, typeof(ContactDataRepository).Name);
+                ioc.IocFillProperty(contacterDal, propertyVal);
+                IMaybeSpecialRepository maybeSpecialDal = ioc.IocConvert<IMaybeSpecialRepository>(dllDir, mvc[MvcLevel.DAL].AssemblyName, mvc[MvcLevel.DAL].Namespace, typeof(MaybeSpecialRepository).Name);
+                ioc.IocFillProperty<IMaybeSpecialRepository>(maybeSpecialDal, propertyVal);
+                ISpecialSpellNameRepository speicalSpellDal = ioc.IocConvert<ISpecialSpellNameRepository>(dllDir, mvc[MvcLevel.DAL].AssemblyName, mvc[MvcLevel.DAL].Namespace, typeof(SpecialSpellNameRepository).Name);
+                ioc.IocFillProperty<ISpecialSpellNameRepository>(speicalSpellDal, propertyVal);
+                IDataFromOtherRepository dataFormOtherDal = ioc.IocConvert<IDataFromOtherRepository>(dllDir, mvc[MvcLevel.DAL].AssemblyName, mvc[MvcLevel.DAL].Namespace, typeof(DataFromOtherRepository).Name);
+                ioc.IocFillProperty<IDataFromOtherRepository>(dataFormOtherDal, propertyVal);
                 #endregion
                 #region orm中dal层实例化存储到字典中
                 propertyVal.Add(typeof(IAppRepository).Name, appDal);
                 propertyVal.Add(typeof(IAppSettingRepository).Name, appSettingDal);
+                propertyVal.Add(typeof(IMenuRepository).Name, menuDal);
+                propertyVal.Add(typeof(IContactDataRepository).Name, contacterDal);
+                propertyVal.Add(typeof(IMaybeSpecialRepository).Name, maybeSpecialDal);
+                propertyVal.Add(typeof(ISpecialSpellNameRepository).Name, speicalSpellDal);
+                propertyVal.Add(typeof(IDataFromOtherRepository).Name, dataFormOtherDal);
                 #endregion
                 #region 业务层 
                 //构造函数的参数注入  判断构造函数的参数是否需要进行注入
-               
+                IAppDataService appService = ioc.IocConvert<IAppDataService>(dllDir, mvc[MvcLevel.DAL].AssemblyName, mvc[MvcLevel.DAL].Namespace, typeof(AppDataService).Name);
+                ioc.IocFillProperty<IAppDataService, AppDataService>(appService, propertyVal);
+                propertyVal.Add(typeof(IAppDataService).Name, appService);
                 IAppSettingService appSetService = ioc.IocConvert<IAppSettingService>(dllDir, mvc[MvcLevel.Bll].AssemblyName, mvc[MvcLevel.Bll].Namespace, typeof(AppSettingService).Name);
                 ioc.IocFillProperty<IAppSettingService, AppSettingService>(appSetService, propertyVal); //属性和字段注入 
                 propertyVal.Add(typeof(IAppSettingService).Name, appSetService);
-                appSetService.QueryWhere(new Model.CategoryItems());
+                IMenuService menuService = ioc.IocConvert<IMenuService>(dllDir, mvc[MvcLevel.Bll].AssemblyName, mvc[MvcLevel.Bll].Namespace, typeof(MenuService).Name);
+                ioc.IocFillProperty<IMenuService, MenuService>(menuService, propertyVal);
+                propertyVal.Add(typeof(IMenuService).Name, menuService);
+                IContactDataService contactService = ioc.IocConvert<IContactDataService>(dllDir, mvc[MvcLevel.Bll].AssemblyName, mvc[MvcLevel.Bll].Namespace, typeof(ContactDataService).Name);
+                ioc.IocFillProperty<IContactDataService, ContactDataService>(contactService, propertyVal);
+                propertyVal.Add(typeof(IContactDataService).Name, contactService);
+                IMaybeSpecialService maybeSpeiclaService = ioc.IocConvert<IMaybeSpecialService>(dllDir, mvc[MvcLevel.Bll].AssemblyName, mvc[MvcLevel.Bll].Namespace, typeof(MaybeSpecialService).Name);
+                ioc.IocFillProperty<IMaybeSpecialService, MaybeSpecialService>(maybeSpeiclaService, propertyVal);
+                propertyVal.Add(typeof(IMaybeSpecialService).Name, maybeSpeiclaService);
+                ISpecialSpellNameService specialSpellService = ioc.IocConvert<ISpecialSpellNameService>(dllDir, mvc[MvcLevel.Bll].AssemblyName, mvc[MvcLevel.Bll].Namespace, typeof(SpecialSpellNameService).Name);
+                ioc.IocFillProperty<ISpecialSpellNameService, SpecialSpellNameService>(specialSpellService, propertyVal);
+                propertyVal.Add(typeof(ISpecialSpellNameService).Name, specialSpellService);
+                IDataFromOtherService dataFormService = ioc.IocConvert<IDataFromOtherService>(dllDir, mvc[MvcLevel.Bll].AssemblyName, mvc[MvcLevel.Bll].Namespace, typeof(DataFromOtherService).Name);
+                ioc.IocFillProperty<IDataFromOtherService, DataFromOtherService>(dataFormService, propertyVal);
+                propertyVal.Add(typeof(IDataFromOtherService).Name, dataFormService);
                 #endregion
             }
         }
