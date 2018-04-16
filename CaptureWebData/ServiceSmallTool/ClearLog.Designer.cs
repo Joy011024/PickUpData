b7cae3e47dd260cedcd16a0a390ecb0d7522db92
hukuolin <hukuolin@wintelia.com>
2018-04-16 12:35:02 +0800

    2018-04-16 日志清理小工具

--- CaptureWebData/ServiceSmallTool/ClearLog.Designer.cs
+++ CaptureWebData/ServiceSmallTool/ClearLog.Designer.cs
@@ -28,20 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnClearLog = new System.Windows.Forms.Button();
+            this.lblLogDir = new System.Windows.Forms.Label();
+            this.txtLogDir = new System.Windows.Forms.TextBox();
+            this.lblDayBefore = new System.Windows.Forms.Label();
+            this.txtDayBefore = new System.Windows.Forms.TextBox();
+            this.lblDayBeforeExt = new System.Windows.Forms.Label();
+            this.lstNote = new System.Windows.Forms.ListView();
+            this.btnClearNote = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnClearLog
+            // 
+            this.btnClearLog.Location = new System.Drawing.Point(240, 58);
+            this.btnClearLog.Name = "btnClearLog";
+            this.btnClearLog.Size = new System.Drawing.Size(75, 23);
+            this.btnClearLog.TabIndex = 0;
+            this.btnClearLog.Text = "清理";
+            this.btnClearLog.UseVisualStyleBackColor = true;
+            // 
+            // lblLogDir
+            // 
+            this.lblLogDir.AutoSize = true;
+            this.lblLogDir.Location = new System.Drawing.Point(12, 16);
+            this.lblLogDir.Name = "lblLogDir";
+            this.lblLogDir.Size = new System.Drawing.Size(53, 12);
+            this.lblLogDir.TabIndex = 1;
+            this.lblLogDir.Text = "日志目录";
+            // 
+            // txtLogDir
+            // 
+            this.txtLogDir.Location = new System.Drawing.Point(92, 13);
+            this.txtLogDir.Name = "txtLogDir";
+            this.txtLogDir.Size = new System.Drawing.Size(318, 21);
+            this.txtLogDir.TabIndex = 2;
+            // 
+            // lblDayBefore
+            // 
+            this.lblDayBefore.AutoSize = true;
+            this.lblDayBefore.Location = new System.Drawing.Point(12, 61);
+            this.lblDayBefore.Name = "lblDayBefore";
+            this.lblDayBefore.Size = new System.Drawing.Size(53, 12);
+            this.lblDayBefore.TabIndex = 3;
+            this.lblDayBefore.Text = "清除时限";
+            // 
+            // txtDayBefore
+            // 
+            this.txtDayBefore.Location = new System.Drawing.Point(92, 58);
+            this.txtDayBefore.Name = "txtDayBefore";
+            this.txtDayBefore.Size = new System.Drawing.Size(75, 21);
+            this.txtDayBefore.TabIndex = 4;
+            this.txtDayBefore.Text = "5";
+            // 
+            // lblDayBeforeExt
+            // 
+            this.lblDayBeforeExt.AutoSize = true;
+            this.lblDayBeforeExt.Location = new System.Drawing.Point(173, 61);
+            this.lblDayBeforeExt.Name = "lblDayBeforeExt";
+            this.lblDayBeforeExt.Size = new System.Drawing.Size(41, 12);
+            this.lblDayBeforeExt.TabIndex = 5;
+            this.lblDayBeforeExt.Text = "天之前";
+            // 
+            // lstNote
+            // 
+            this.lstNote.Location = new System.Drawing.Point(14, 102);
+            this.lstNote.Name = "lstNote";
+            this.lstNote.Size = new System.Drawing.Size(396, 107);
+            this.lstNote.TabIndex = 6;
+            this.lstNote.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnClearNote
+            // 
+            this.btnClearNote.Location = new System.Drawing.Point(335, 58);
+            this.btnClearNote.Name = "btnClearNote";
+            this.btnClearNote.Size = new System.Drawing.Size(75, 23);
+            this.btnClearNote.TabIndex = 7;
+            this.btnClearNote.Text = "清档";
+            this.btnClearNote.UseVisualStyleBackColor = true;
             // 
             // ClearLog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(422, 221);
+            this.Controls.Add(this.btnClearNote);
+            this.Controls.Add(this.lstNote);
+            this.Controls.Add(this.lblDayBeforeExt);
+            this.Controls.Add(this.txtDayBefore);
+            this.Controls.Add(this.lblDayBefore);
+            this.Controls.Add(this.txtLogDir);
+            this.Controls.Add(this.lblLogDir);
+            this.Controls.Add(this.btnClearLog);
             this.Name = "ClearLog";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnClearLog;
+        private System.Windows.Forms.Label lblLogDir;
+        private System.Windows.Forms.TextBox txtLogDir;
+        private System.Windows.Forms.Label lblDayBefore;
+        private System.Windows.Forms.TextBox txtDayBefore;
+        private System.Windows.Forms.Label lblDayBeforeExt;
+        private System.Windows.Forms.ListView lstNote;
+        private System.Windows.Forms.Button btnClearNote;
     }
 }
 
--- CaptureWebData/ServiceSmallTool/ClearLog.cs
+++ CaptureWebData/ServiceSmallTool/ClearLog.cs
@@ -7,7 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using Infrastructure.ExtService;
+using System.IO;
 namespace ServiceSmallTool
 {
     public partial class ClearLog : Form
@@ -15,6 +16,69 @@ namespace ServiceSmallTool
         public ClearLog()
         {
             InitializeComponent();
+            txtLogDir.Text = ExeDir;
+            btnClearLog.Click += new EventHandler(Button_Click);
+        }
+        string ExeDir
+        {
+            get 
+            {
+                return new AppDirHelper().GetAppDir(AppCategory.WinApp);
+            }
+        }
+        public void Button_Click(object sender,EventArgs e)
+        {
+            string day = txtDayBefore.Text;
+            string dir = txtLogDir.Text;
+            int DayBefore = 0;
+            int.TryParse(day, out DayBefore);
+            if (DayBefore < 0)
+            {
+                DayBefore = 1;
+            }
+            ClearLogHelp help = new ClearLogHelp();
+            help.ClearLogOfDayBefore(dir, DayBefore);
+        }
+    }
+    public class ClearLogHelp 
+    {
+        public void ClearLogOfDayBefore(string logDir, int dayBefore) 
+        {
+            /*
+             清理指定日期前的日志
+             * 步骤A
+             * 1.查找目录下的文件
+             * 2.文件创建日期是在指定日期前的进行删除操作
+            */
+            DirectoryInfo di = new DirectoryInfo(logDir);
+            FileInfo[] fis = di.GetFiles();//目录下的文件列表
+            if (fis.Length == 0)
+            {//空文件夹 
+            
+            }
+            DateTime time = DateTime.Now.AddDays(-dayBefore);
+            for (int i = 0; i < fis.Length; i++)
+            {
+                FileInfo item = fis[i];
+                if (item.CreationTime < time)
+                {
+                    item.Delete();
+                }
+            }
+            /*
+             * 步骤B
+             * 子目录 遍历
+             * 重复步骤A
+             */
+            DirectoryInfo[] dis= di.GetDirectories();
+            if (dis.Length == 0)
+            {
+                return;
+            }
+            for (int i = 0; i < dis.Length; i++)
+            {
+                ClearLogOfDayBefore(dis[i].FullName, dayBefore);
+            }
         }
     }
 }
