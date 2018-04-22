dfaafede231f61971d005d6fb54e56770951b22f
hukuolin@outlook.com <2468101214hkl>
2018-04-22 23:33:01 +0800

    2018-04-22 bs增加文件输出功能

--- HRApp/HRApp.Web/Scripts/Common/Common.js
+++ HRApp/HRApp.Web/Scripts/Common/Common.js
@@ -102,6 +102,38 @@ $(function () {
             var exp = new Date();
             exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000);
             document.cookie = key + "=" + escape(value) + ";expires=" + exp.toGMTString();
+        },
+        WriteFile: function (value, type, name) {
+            if ($.trim(type) == '')
+            {
+                type = "text/latex";
+            }
+            var blob;
+            if (typeof window.Blob == "function") {
+                blob = new Blob([value], { type: type });
+            } else {
+                var BlobBuilder = window.BlobBuilder || window.MozBlobBuilder || window.WebKitBlobBuilder || window.MSBlobBuilder;
+                var bb = new BlobBuilder();
+                bb.append(value);
+                blob = bb.getBlob(type);
+            }
+            var URL = window.URL || window.webkitURL;
+            var bloburl = URL.createObjectURL(blob);
+            var anchor = document.createElement("a");
+            if ('download' in anchor) {
+                anchor.style.visibility = "hidden";
+                anchor.href = bloburl;
+                anchor.download = name;
+                document.body.appendChild(anchor);
+                var evt = document.createEvent("MouseEvents");
+                evt.initEvent("click", true, true);
+                anchor.dispatchEvent(evt);
+                document.body.removeChild(anchor);
+            } else if (navigator.msSaveBlob) {
+                navigator.msSaveBlob(blob, name);
+            } else {
+                location.href = bloburl;
+            }
         }
     });
 });
