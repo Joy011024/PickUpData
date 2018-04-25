1e7bbcb0c3793f78d578a823596f87787c2b07fb
hukuolin <hukuolin@wintelia.com>
2018-04-25 14:09:41 +0800

    2018-04-24 操作举报对话框时目标参数定义 usingParentWinSelfTool：是否使用父容器的操作按钮（当为true时其他参数无意义） table:操作对象是归属于哪张数据表， optionId：操作对象的标志id

--- HRApp/HRApp.Web/Controllers/ReportEnumController.cs
+++ HRApp/HRApp.Web/Controllers/ReportEnumController.cs
@@ -13,13 +13,14 @@ namespace HRApp.Web.Controllers
         //
         // GET: /ReportEnum/
         [DescriptionSort("举报对话框 usingParentWinSelfTool=true使用父窗体自定义工具栏")]
-        public ActionResult FlagReportDialog(bool usingParentWinSelfTool,Guid? optionId)
+        public ActionResult FlagReportDialog(bool usingParentWinSelfTool,Guid? optionId,string table)
         {
             IAppSettingService service = IocMvcFactoryHelper.GetInterface<IAppSettingService>();
             string item=EAppSetting.ReportEnum.ToString();
             ViewData[item] = service.SelectNodeItemByParentCode(item);
             ViewData["usingParentWinSelfTool"] = usingParentWinSelfTool;
             ViewData["optionId"] = optionId;
+            ViewData["optionTable"] = table;
             return View();
         }
 
