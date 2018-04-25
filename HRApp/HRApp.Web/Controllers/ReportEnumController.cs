032d3f56ecae6b6a323b71106659ff31345fcae0
hukuolin <hukuolin@wintelia.com>
2018-04-25 10:48:30 +0800

    2014-04-25 举报对话框

---
+++ HRApp/HRApp.Web/Controllers/ReportEnumController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Domain.CommonData;
+using HRApp.IApplicationService;
+using HRApp.Model;
+namespace HRApp.Web.Controllers
+{
+    public class ReportEnumController : BaseController
+    {
+        //
+        // GET: /ReportEnum/
+        [DescriptionSort("举报对话框")]
+        public ActionResult FlagReportDialog()
+        {
+            IAppSettingService service = IocMvcFactoryHelper.GetInterface<IAppSettingService>();
+            string item=EAppSetting.ReportEnum.ToString();
+            ViewData[item] = service.SelectNodeItemByParentCode(item);
+            return View();
+        }
+
+    }
+}
