1ad0f285a831dfe559e8894143a65a3541d26ef8
hukuolin <hukuolin@wintelia.com>
2018-04-26 17:09:44 +0800

    2018-04-26 创建举报数据的数据存储逻辑层，数据访问层服务类

---
+++ HRApp/HRApp.ApplicationService/ReportEnumDataService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HRApp.Model;
+using IHRApp.Infrastructure;
+using HRApp.IApplicationService;
+namespace HRApp.ApplicationService
+{
+    public class ReportEnumDataService:IReportEnumDataService
+    {
+        public string SqlConnString
+        {
+            get;
+            set;
+        }
+
+        public Common.Data.JsonData Add(ReportEnumDetail model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ReportEnumDetail> QueryWhere(ReportEnumDetail model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HRApp/HRApp.Infrastructure/ReportEnumDataRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HRApp.Model;
+using IHRApp.Infrastructure;
+namespace HRApp.Infrastructure
+{
+    public class ReportEnumDataRepository:IReportEnumDataRepository
+    {
+        public IList<ReportEnumDetail> QueryList(string cmd, Dictionary<string, object> param)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ReportEnumDetail> QueryAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string SqlConnString
+        {
+            get;
+            set;
+        }
+
+        public bool Add(ReportEnumDetail entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Edit(ReportEnumDetail entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(object key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool LogicDel(object key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ReportEnumDetail Get(object key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ReportEnumDetail> Query(string cmd)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
