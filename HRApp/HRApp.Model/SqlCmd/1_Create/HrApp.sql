ba0b60733b77dcb21daf23109e389c9974f23ce1
hukuolin <hukuolin@wintelia.com>
2018-04-26 15:54:41 +0800

    2018-04-26 举报信息内容描述存储表

--- HRApp/HRApp.Model/SqlCmd/1_Create/HrApp.sql
+++ HRApp/HRApp.Model/SqlCmd/1_Create/HrApp.sql
@@ -143,4 +143,14 @@ create table ReportEnumRec
 	CreateTime datetime not null,
 	Reporter varchar(16) not null,--sys flag ,set the value=-1
 	IsDelete bit not null
-)
+);
+create table ReportEnumRecNote
+(
+	Id uniqueidentifier primary key,
+	ReportId uniqueidentifier not null,
+	CreateTime datetime not null,
+	Note nvarchar(256) not null,
+	IsDelete bit not null
+);
+alter table ReportEnumRecNote add constraint fj_ReportId foreign key (ReportId) 
+ references ReportEnumRec(Id);
