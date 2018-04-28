e217b344c1368bb24e18ea55a6187919a7b5faf5
hukuolin <hukuolin@wintelia.com>
2018-04-28 15:10:00 +0800

    2018-04-28 数据访问层准备操作表-列依赖关系

--- HRApp/HRApp.Model/Entity/RelyTable.cs
+++ HRApp/HRApp.Model/Entity/RelyTable.cs
@@ -14,5 +14,20 @@ namespace HRApp.Model
         public string Note { get; set; }
         public bool IsPrimaryKey { get; set; }
         public bool IsDelete { get; set; }
+        public string GetInitDBRelactiveSql() 
+        {
+            return @"insert   [dbo].[RelyTable] (TableName,ColumnName,CreateTime,IsDelete,IsPrimaryKey)
+select tab.name, cell.name,GETDATE(),0,0 from sys.tables  tab ,sys.columns  cell
+where  cell.object_id=object_id(tab.name) and tab.name not in (select TableName from RelyTable)
+and cell.name not in  (select  ColumnName from RelyTable)";
+        }
+        /// <summary>
+        /// 简单的查询语句
+        /// </summary>
+        /// <returns></returns>
+        public string GetSampleQuerySql() 
+        {
+            return @"select TableName,ColumnName,CreateTime,IsDelete,IsPrimaryKey from [dbo].[RelyTable] ";
+        }
     }
 }
