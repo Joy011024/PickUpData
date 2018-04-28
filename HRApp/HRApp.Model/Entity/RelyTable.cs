758c7058ed2b33271204e7a01f4b63a68f6851d3
hukuolin <hukuolin@wintelia.com>
2018-04-28 14:46:54 +0800

    2018-04-28 存储主键，已经索引键的数据表业务查询【实体准备】

--- HRApp/HRApp.Model/Entity/RelyTable.cs
+++ HRApp/HRApp.Model/Entity/RelyTable.cs
@@ -9,6 +9,10 @@ namespace HRApp.Model
 {
     public class RelyTable : IntPrimaryKey
     {
-
+        public string TableName { get; set; }
+        public string ColumnName { get; set; }
+        public string Note { get; set; }
+        public bool IsPrimaryKey { get; set; }
+        public bool IsDelete { get; set; }
     }
 }
