5e903ee5b77a141eeb34f00fbf62f723c2ae22db
hukuolin <hukuolin@wintelia.com>
2018-04-28 14:22:29 +0800

    2018-04-28 表，列关系进行实体化准备

---
+++ HRApp/HRApp.Model/Entity/RelyTable.cs
@@ -0,0 +1,14 @@
+﻿using Domain.CommonData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRApp.Model
+{
+    public class RelyTable : IntPrimaryKey
+    {
+
+    }
+}
