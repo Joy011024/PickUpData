28f65e44c144635c71112aa909571b6cfdf02bd8
hukuolin <hukuolin@wintelia.com>
2018-05-15 17:04:21 +0800

    2018-05-15 邮件发送接口【1】

--- HRApp/HRApp.Web/Controllers/EmailDataController.cs
+++ HRApp/HRApp.Web/Controllers/EmailDataController.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using HRApp.Model;
+using HRApp.IApplicationService;
 namespace HRApp.Web.Controllers
 {
     public class EmailDataController : BaseController
@@ -15,9 +16,12 @@ namespace HRApp.Web.Controllers
         {
             return View();
         }
-        public void CallEmailService() 
+        public void CallEmailService(AppEmailData email) 
         {
-            
+            //查询邮件账户
+            EmailSystemSetting setting=new EmailSystemSetting();
+            IEmailDataService es = IocMvcFactoryHelper.GetInterface<IEmailDataService>();
+            //es.SendEmail(setting,email)
         }
     }
 }
