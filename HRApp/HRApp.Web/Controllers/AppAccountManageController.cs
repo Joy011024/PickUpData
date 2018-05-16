c721b42c82d40283aa66081a1b0c0b1b2c3cec85
hukuolin <hukuolin@wintelia.com>
2018-05-16 18:23:03 +0800

    2018-05-16 初始化 账号列表【1】

---
+++ HRApp/HRApp.Web/Controllers/AppAccountManageController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HRApp.Web.Controllers
+{
+    public class AppAccountManageController : Controller
+    {
+        //
+        // GET: /AppAccountManage/
+        [Description("账号服务列表")]
+        public ActionResult AccountList()
+        {
+            return View();
+        }
+        [Description("注册账号")]
+        public ActionResult SignInAccount() 
+        {
+            return View();
+        }
+    }
+}
