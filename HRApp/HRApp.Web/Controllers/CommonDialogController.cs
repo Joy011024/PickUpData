859dde5e7317b34451cde2e98675c37c56d38768
hukuolinpc@outlook.com <hukuolin@outlook.com>
2018-05-17 23:38:48 +0800

    2018-05-17 通用 名称-助记码 对话框

---
+++ HRApp/HRApp.Web/Controllers/CommonDialogController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HRApp.Web.Controllers
+{
+    public class CommonDialogController : Controller
+    {
+        //
+        // GET: /CommonDialog/
+        [Description("公用对话框")]
+        public ActionResult SampleDataDialog()
+        {
+            return View();
+        }
+
+    }
+}
