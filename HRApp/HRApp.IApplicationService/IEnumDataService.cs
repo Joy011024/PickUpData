4c1a736a3a87fdbf062dc03688b044a6257192f8
hukuolinpc@outlook.com <hukuolin@outlook.com>
2018-05-21 22:49:32 +0800

    2018-05-21 新增功能 ：日志输出到库，枚举信息库存储

---
+++ HRApp/HRApp.IApplicationService/IEnumDataService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRApp.IApplicationService
+{
+    public interface IEnumDataService
+    {
+    }
+}
