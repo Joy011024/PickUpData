7ce182daf50d252c81c8570b7116f5bd24a61f2c
hukuolin <hukuolin@wintelia.com>
2018-05-24 17:59:56 +0800

    2018-05-24 增加datatable与实体定义反射匹配关系函数

--- BaseHelper/DataHelp/DataReflection.cs
+++ BaseHelper/DataHelp/DataReflection.cs
@@ -37,6 +37,10 @@ namespace DataHelp
             {
                 PropertyInfo p = s.GetProperty(item);
                 object value = p.GetValue(soure,null);
+                if (value == null)
+                {
+                    continue;
+                }
                 PropertyInfo tp = r.GetProperty(item);
                 MemberInfo mi= tp.GetSetMethod();
                 if (mi == null) { continue; }
@@ -81,6 +85,10 @@ namespace DataHelp
             {
                 PropertyInfo p = s.GetProperty(sourceField[i]);
                 object value = p.GetValue(soure, null);
+                if (value == null)
+                {
+                    continue;
+                }
                 PropertyInfo tp = r.GetProperty(resultFiled[i]);
                 MemberInfo mi = tp.GetSetMethod();
                 if (mi == null) { continue; }
@@ -92,17 +100,16 @@ namespace DataHelp
         {
             DataTable table = ds.Tables[0];
             DataColumnCollection columns = table.Columns;
-            List<string> modelFiled = new List<string>();
-            List<string> tableFields = new List<string>();
+            Dictionary<string, string> map = new Dictionary<string, string>();
             PropertyInfo[] pis = typeof(T).GetProperties();
             string[] pns=pis.Select(s=>s.Name.ToLower()).ToArray();
+            List<T> list = new List<T>();
             //查找出公告的列
             foreach (DataColumn item in columns)
             {
                 if (pns.Contains(item.ColumnName.ToLower()))
                 {
-                    tableFields.Add(item.ColumnName);
-                    modelFiled.Add(pis.Where(p => p.Name.ToLower() == item.ColumnName.ToLower()).FirstOrDefault().Name);
+                    map.Add(pis.Where(p => p.Name.ToLower() == item.ColumnName.ToLower()).FirstOrDefault().Name, item.ColumnName);
                 }
                 else
                 { //移除非查询列
@@ -113,7 +120,8 @@ namespace DataHelp
             {
                 T d = System.Activator.CreateInstance<T>();
                 //根据匹配的列进行选择设置
-
+                FillRowIntoEntity(d, item, map);
+                list.Add(d);
             }
             
         }
@@ -199,6 +207,82 @@ namespace DataHelp
                 items.Add(entity);
             }
             return items;
+        }
+        public static List<T> DataSetConvert<T>(this DataTable table, Dictionary<string, string> columnMapProperty) where T : class
+        {
+            List<T> data = new List<T>();
+            DataColumnCollection columns=table.Columns;
+            string[] convert = columnMapProperty.Keys.Select(s=>s.ToLower()).ToArray();
+            string[] key = columnMapProperty.Keys.ToArray();
+            Dictionary<string, string> propertyMapColumn = new Dictionary<string, string>();//属性匹配列
+            foreach (DataColumn item in columns)
+            {//移除无关数据
+                if (!convert.Contains(item.ColumnName.ToLower()))
+                {//转换为小写匹配
+                    table.Columns.Remove(item);
+                    continue;
+                }
+                //收集匹配关系
+                int index = Array.IndexOf(convert, item.ColumnName.ToLower());
+                propertyMapColumn.Add( columnMapProperty[key[index]],item.ColumnName);
+            }
+            foreach (DataRow item in table.Rows)
+            {
+                T rec = System.Activator.CreateInstance<T>();
+                FillRowIntoEntity(rec, item, propertyMapColumn);
+                data.Add(rec);
+            }
+            return data;
+        }
+        /// <summary>
+        /// 数据填充到实体
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="rec"></param>
+        /// <param name="row"></param>
+        /// <param name="propertyMapcolumn">属性匹配列的关系字典</param>
+        static void FillRowIntoEntity<T>(T rec, DataRow row, Dictionary<string, string> propertyMapcolumn)
+        {
+            Type targetClass=rec.GetType();
+            foreach (var item in propertyMapcolumn)
+            {
+                object column = row[item.Value];
+                if (column == null)
+                {
+                    continue;  
+                }
+                PropertyInfo pi = targetClass.GetProperty(item.Key);
+                SetValueIntoProperty(pi, rec, column);
+            }
+        }
+        /// <summary>
+        /// 将数据写入到实体中
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="pi"></param>
+        /// <param name="entity"></param>
+        /// <param name="value"></param>
+        static void SetValueIntoProperty<T>(PropertyInfo pi, T entity,object value)
+        {
+            if (value == null)
+            {
+                return;
+            }
+            if (pi.PropertyType.Name == typeof(Nullable<>).Name)
+            {
+                NullableConverter nullableConverter = new NullableConverter(pi.PropertyType);//如何获取可空类型属性非空时的数据类型
+                Type nt = nullableConverter.UnderlyingType;
+                pi.SetValue(entity, Convert.ChangeType(value, nt), null);
+            }
+            else if (value.GetType().Name != typeof(DBNull).Name && value.GetType().Name != pi.PropertyType.Name)
+            {
+                object pv = Convert.ChangeType(value, pi.PropertyType);
+                pi.SetValue(entity, pv, null);
+            }
+            else if (value.GetType().Name != typeof(DBNull).Name) //如果此字段数据为空是的处理
+            {
+                pi.SetValue(entity, value, null);
+            }
         }
     }
 }
