ef42c3db6a4f402dd28a1f5becdcc8f4fed09d76
hukuolin <hukuolin@wintelia.com>
2018-05-31 18:10:53 +0800

    2018-05-31 完善人员数据

--- CaptureData/Infrastructure/Domain/Domain.CommonData/SqlCmd/AccountDBSqlCmd/SqlCmd/4_LogicData/4_0_LogicData.sql
+++ CaptureData/Infrastructure/Domain/Domain.CommonData/SqlCmd/AccountDBSqlCmd/SqlCmd/4_LogicData/4_0_LogicData.sql
@@ -1,3 +1,10 @@
 USE    AccountDB
 go
 INSERT [dbo].[User] ([Id], [UserName], [Nick], [CreateTime], [Psw], [Encry], [IsActive]) VALUES (N'491d92ca-43b7-46fb-8be3-3872168077e3', N'SuperAdmin', N'管鲤鱼', CAST(0x0000A8F000032968 AS DateTime), N'123456', N'-1', 1)
+INSERT [dbo].[User] ([Id], [UserName], [Nick], [CreateTime], [Psw], [Encry], [IsActive]) VALUES (N'82fbb42d-997c-4d39-8880-0137990586e3', N'StaticAdmin', N'同济大师', CAST(0x0000A8F1012A65E2 AS DateTime), N'123456', N'-1', 1)
+INSERT [dbo].[User] ([Id], [UserName], [Nick], [CreateTime], [Psw], [Encry], [IsActive]) VALUES (N'89df8cf6-a150-4d83-8aca-0aaf2de6cfbe', N'InputAdmin', N'疏儒道长', CAST(0x0000A8F10129E2FB AS DateTime), N'123456', N'-1', 1)
+INSERT [dbo].[User] ([Id], [UserName], [Nick], [CreateTime], [Psw], [Encry], [IsActive]) VALUES (N'787bc175-4f99-46ce-9bc5-1501ea6565d2', N'ViewAdmin', N'静心道长', CAST(0x0000A8F101298ED4 AS DateTime), N'123456', N'-1', 1)
+INSERT [dbo].[User] ([Id], [UserName], [Nick], [CreateTime], [Psw], [Encry], [IsActive]) VALUES (N'e7daae04-934a-4e09-b7a5-1a98d88a41be', N'AdultAdmin', N'沈管', CAST(0x0000A8F1011C42FA AS DateTime), N'123456', N'-1', 1)
+INSERT [dbo].[User] ([Id], [UserName], [Nick], [CreateTime], [Psw], [Encry], [IsActive]) VALUES (N'e42f1087-5920-48ed-af85-b81e71613c22', N'FlowAdmin', N'刘专', CAST(0x0000A8F0009A19F6 AS DateTime), N'123456', N'-1', 1)
+INSERT [dbo].[User] ([Id], [UserName], [Nick], [CreateTime], [Psw], [Encry], [IsActive]) VALUES (N'16789032-a2a2-4854-a6bc-bf41c0742b54', N'OutputAdmin', N'疏黜道长', CAST(0x0000A8F1012A2ED1 AS DateTime), N'123456', N'-1', 1)
+INSERT [dbo].[User] ([Id], [UserName], [Nick], [CreateTime], [Psw], [Encry], [IsActive]) VALUES (N'09fcecfe-1b69-4fe6-b500-f22dd511a801', N'LogicAdmin', N'罗技源', CAST(0x0000A8F00099DE02 AS DateTime), N'123456', N'-1', 1)
