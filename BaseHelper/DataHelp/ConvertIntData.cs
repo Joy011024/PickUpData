eeb22b1d99dd907bfb573af60063177367cc47d9
hukuolin <hukuolin@wintelia.com>
2018-06-15 15:36:05 +0800

    2018-06-15 增加十进制的位运算函数，10进制转2进制输出完整字符串函数

--- BaseHelper/DataHelp/ConvertIntData.cs
+++ BaseHelper/DataHelp/ConvertIntData.cs
@@ -46,5 +46,65 @@ namespace DataHelp
                 return null;
             }
         }
+		/// <summary>
+        /// 输出标准2进制字符串
+        /// </summary>
+        /// <param name="value10"></param>
+        public static string Conver2String(int value10) 
+        {
+            string value2 = Convert.ToString(value10, 2);
+            //不足前置像补0 （4的倍数）
+            int len = value2.Length;//当前位数
+            int loseLen =4- len % 4;
+            return value2.PadLeft(len+loseLen, '0');
+        }
+        public static string Calculate2And(string first,string second) 
+        {
+            string min=string.Empty, max=string.Empty;
+            #region 避免出现长度不一致的情形
+            if (first.Length > second.Length)
+            {
+                min = second;
+                max = first;
+            }
+            else 
+            {
+                min = first;
+                max = second;
+            }
+            #endregion
+            List<char> outPut = new List<char>();
+            for (int i = max.Length-1; i >= 0; i--)
+            {
+                if (max.Length - i > min.Length)
+                {
+                    break;
+                }
+                string fc = max.Substring(i, 1);
+                string sc = min.Substring(i, 1);
+                if (sc == fc && fc == "1")
+                {
+                    outPut.Insert(0, '1');
+                }
+                else {
+                    outPut.Insert(0, '0');
+                }
+            }
+            return string.Join("", outPut);
+        }
+        /// <summary>
+        /// 10进制数进行位运算输出运算后10进制结果
+        /// </summary>
+        /// <param name="first"></param>
+        /// <param name="second"></param>
+        /// <returns></returns>
+        public static int Calculate2AndToInt(int first, int second)
+        {
+            //数字进行位运算，并转化为10进制结果
+            string first2 = Conver2String(first);
+            string second2 = Conver2String(second);
+            string cal = Calculate2And(first2, second2);
+            return Convert.ToInt32(cal, 2);
+        }
     }
 }
