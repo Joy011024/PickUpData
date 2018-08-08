2e2c5879591b4102bc621e171b34d33900d9af6b
hukuolinpc@outlook.com <hukuolin@outlook.com>
2018-08-09 00:11:54 +0800

    初始化sqlite脚本

---
+++ CaptureData/Infrastructure/Domain/Domain.CommonData/SqlCmd/SQLite/Create/0_Create.sql
@@ -0,0 +1,23 @@
+CREATE TABLE TecentQQData (
+    ID                   VARCHAR (36)  PRIMARY KEY,
+    PickUpWhereId        VARCHAR (36),
+    age                  INT,
+    city                 VARCHAR (50),
+    country              VARCHAR (50),
+    distance             INT,
+    face                 INT,
+    gender               INT,
+    nick                 VARCHAR (50),
+    province             VARCHAR (50),
+    stat                 INT,
+    uin                  VARCHAR (15),
+    HeadImageUrl         VARCHAR (500),
+    CreateTime           DATETIME,
+    IsGatherImage        BLOB          DEFAULT (0),
+    GatherImageTime      DATETIME,
+    LastErrorGatherImage DATETIME,
+    GatherImageErrorNum  INT           DEFAULT (0),
+    ImageRelativePath    VARCHAR (200),
+    DayInt               INT           DEFAULT (0),
+    ImgType              INT           DEFAULT (0) 
+);
