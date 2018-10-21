6a4559610db99f5318937eb2544a3b8d177e0d93
hukuolinpc@outlook.com <hukuolin@outlook.com>
2018-10-21 23:42:18 +0800

    ef扩展

---
+++ CaptureData/Infrastructure/Infrastructure.EFSQLite/SQLiteReporistory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace Infrastructure.EFSQLite
+{
+    public class SQLiteReporistory<T> : System.Data.Entity.DbContext where T : class
+    {
+        string conn;
+        public SQLiteReporistory(string connString)
+            : base(connString)
+        {
+            conn = connString;
+        }
+        protected override void OnModelCreating(System.Data.Entity.DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        public System.Data.Entity.IDbSet<T> Entity { get; set; }
+        public void BatchAdd(List<T> data)
+        {
+            try
+            {
+                foreach (var item in data)
+                {
+                    Entity.Add(item);
+                }
+                int succ = SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
