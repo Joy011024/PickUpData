f86e6849d28130b0be06b920150feab8aeb60d56
158055983@qq.com <158055983@qq.com>
2018-12-03 19:37:51 +0800

    抽象的服务层调用接口类库：开始进行数据库分类拆分支持

---
+++ CaptureWebData/ApplicationService.IDataService/ICategroyService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.CommonData;
+namespace ApplicationService.IDataService
+{
+    public interface  ICategroyService
+    {
+        string ConnString { get; set; }
+        IEnumerable<CategoryData> QueryCityCategory();
+    }
+}
