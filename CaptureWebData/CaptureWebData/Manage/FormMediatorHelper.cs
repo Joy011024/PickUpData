8bf91fe989c314b3fd8fede184f4a5c38a345e92
hukuolinpc@outlook.com <hukuolin@outlook.com>
2019-04-09 22:03:45 +0800

    compare the extend form invalid

--- CaptureWebData/CaptureWebData/Manage/FormMediatorHelper.cs
+++ CaptureWebData/CaptureWebData/Manage/FormMediatorHelper.cs
@@ -10,13 +10,10 @@ namespace CaptureWebData
     public class FormMediatorHelper : Form, IMediator, INotifier
     {
         #region private member
-        protected object compontent;
         protected string mediatorName;
+        protected object compontent;
         #endregion
-        public FormMediatorHelper() : this("mediator")
-        {
-
-        }
+       
         public FormMediatorHelper(string mediatorName)
         {
             this.mediatorName = mediatorName;
@@ -24,39 +21,50 @@ namespace CaptureWebData
             FormBorderStyle = FormBorderStyle.None;
         }
         #region override
+        public virtual string MediatorName
+        {
+            get
+            {
+                return this.mediatorName;
+
+            }
+        }
+        public virtual object ViewComponent
+        {
+            get => compontent;
+            set => compontent = value;
+        }
+        protected IFacade Facade
+        {
+            get => facade;
+        }
         private IFacade facade = CommonFacade.FacadeInstance;
-        public virtual string MediatorName => this.mediatorName;
-        protected IFacade Facade { get => facade; }
-
-        public virtual object ViewComponent { get => compontent; set => compontent=value; }
-
-        public virtual void HandleNotification(INotification notification)
-        {
-            
-        }
-        public virtual void InitializeNotifier(string key)
-        {
-
-        }
 
         public virtual string[] ListNotificationInterests()
         {
             return new string[] { };
         }
-
+        public virtual void HandleNotification(INotification notification)
+        {
+            
+        }
         public virtual void OnRegister()
         {
-            
-        }
 
+        }
         public virtual void OnRemove()
         {
-            
-        }
 
+        }
+        
         public virtual void SendNotification(string notificationName, object body = null, string type = null)
         {
             facade.SendNotification(notificationName, body, type);
+        }
+
+        public virtual void InitializeNotifier(string key)
+        {
+
         }
         #endregion
 
@@ -64,43 +72,43 @@ namespace CaptureWebData
 
         #region Constants
         #endregion
- 
+
 
         #region Constructors
-       
 
-       
+
+
         #endregion
 
         #region Accessors
-       
 
-        
 
-        
+
+
+
         #endregion
 
-        
+
 
         #region Public Methods
         #region IMediator Members
-         
 
-        
 
-       
 
-        
+
+
+
+
         #endregion
 
         #region INotifier Members
 
-       
 
-       
 
-       
-       
+
+
+
+
         #endregion
         #endregion
 
