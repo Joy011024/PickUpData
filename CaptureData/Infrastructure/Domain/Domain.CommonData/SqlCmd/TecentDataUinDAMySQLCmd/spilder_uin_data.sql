51c20b042ce41148b74bdd783db820c920ad49bd
admin <hukuolin@outlook.com>
2019-05-16 23:42:24 +0800

    mysql  创建表  （MySQL数据库字符集会影响外键创建关联表名是否忽略大小写）

--- CaptureData/Infrastructure/Domain/Domain.CommonData/SqlCmd/TecentDataUinDAMySQLCmd/spilder_uin_data.sql
+++ CaptureData/Infrastructure/Domain/Domain.CommonData/SqlCmd/TecentDataUinDAMySQLCmd/spilder_uin_data.sql
@@ -13,7 +13,7 @@ create table if not exists  `app_config`
 	`name` varchar(32) not null,
 	`cfg_key` varchar(32) not null,
 	`cfg_desc` varchar(512),
-	`create_time` datetime,
+	`create_time` datetime not null,
 	`cfg_value` varchar(32) not null,
 	`parent_id` smallint 
 );
@@ -23,8 +23,8 @@ create table if not exists `app_ver`
 	`app_version` varchar(32) not null,
 	`app_id` int not null,
 	`is_now_version` tinyint not null, # equal bool
-	`create_time` datetime ,
-	constraint `fk_id` foreign key (`app_id`) references `app` (`id`)
+	`create_time` datetime not null,
+	constraint `fk_app_id` foreign key (`app_id`) references `App` (`id`)
 );
 create table if not exists  `Category_data`
 (
